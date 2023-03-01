c2d61e87f4f5fcee14387f8de7cac376b385f3ac
Nikolay Tankov <tankov.n.k@gmail.com>
2023-03-01 13:08:30 +0200

    mark

--- src/components/CardTwit/CardTwit.jsx
+++ src/components/CardTwit/CardTwit.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 // import css from './ContactsForm.module.css';
-import { Container } from './CardTwit.styled';
+import {
+  Container,
+  BoxLine,
+  ImgCentral,
+  HeaderTweet,
+  HeaderFollowers,
+  BtnFollow,
+} from './CardTwit.styled';
 import Header from '../../img/picture-header.png';
+import FotoUser from '../../img/picture-boy.png';
 
 function CardTwit() {
   return (
     <Container>
       <img src={Header} alt="Header" />
+      <ImgCentral src={FotoUser} alt="Header" />
+      <BoxLine></BoxLine>
+      <HeaderTweet>777 TWETS</HeaderTweet>
+      <HeaderFollowers>100,500 FOLLOWERS</HeaderFollowers>
+      <BtnFollow>FOLLOW</BtnFollow>
     </Container>
   );
 }
--- src/components/CardTwit/CardTwit.styled.jsx
+++ src/components/CardTwit/CardTwit.styled.jsx
@@ -1,7 +1,11 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+  position: relative;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 380px;
   height: 460px;
   background-color: red;
@@ -16,4 +20,60 @@ export const Container = styled.div`
   padding-top: 28px;
   padding-left: 36px;
   padding-right: 36px;
+`;
+
+export const BoxLine = styled.div`
+  position: absolute;
+  left: 0;
+  top: 214px;
+  width: 380px;
+  height: 8px;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
+    inset 0px -1.71846px 3.43693px #ae7be3, inset 0px 3.43693px 2.5777px #fbf8ff;
+  @media screen and (min-width: 1280px) {
+  }
+`;
+
+export const ImgCentral = styled.img`
+  position: absolute;
+  left: 150px;
+  top: 178px;
+  height: 80px;
+  width: 80px;
+  padding: 0;
+  z-index: 10;
+`;
+
+export const HeaderTweet = styled.h2`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 1.2;
+  text-transform: uppercase;
+  color: #ebd8ff;
+  margin-top: 88px;
+  text-align: center;
+`;
+
+export const HeaderFollowers = styled.h2`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+  color: #ebd8ff;
+  margin-top: 16px;
+  text-align: center;
+`;
+
+export const BtnFollow = styled.button`
+  width: 196px;
+  height: 50px;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10.3108px;
+  margin-top: 26px;
 `;
