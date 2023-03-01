b014661e77619ac3a741db2135f752fb71a2044f
Nikolay Tankov <tankov.n.k@gmail.com>
2023-03-01 11:53:36 +0200

    //

---
+++ src/components/CardTwit/CardTwit.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import css from './ContactsForm.module.css';
+import { Container } from './CardTwit.styled';
+
+function CardTwit() {
+  //   const handleSubmit = (values, { resetForm }) => {
+  //     onSubmit(values);
+  //     resetForm();
+  //   };
+
+  return <Container>1</Container>;
+}
+
+export default CardTwit;
